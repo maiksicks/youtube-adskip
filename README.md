e33428b218d18976d8d566df0d3f1f8713078fbf
maiksicks <44972160+maiksicks@users.noreply.github.com>
2023-11-14 09:11:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,37 @@
+# AdSkip
+Skip YouTube Ads instead of blocking them.
+
+## How to install
+
+- (Chrome)[https://developer.chrome.com/docs/extensions/mv3/getstarted/development-basics/#load-unpacked]
+- Firefox (untested)
+- Opera (untested)
+- Edge (untested)
+
+## Development
+
+### Install
+
+	$ npm install
+
+### Development
+
+    npm run dev chrome
+    npm run dev firefox
+    npm run dev opera
+    npm run dev edge
+
+### Build
+
+    npm run build chrome
+    npm run build firefox
+    npm run build opera
+    npm run build edge
+
+### Environment
+
+The build tool also defines a variable named `process.env.NODE_ENV` in your scripts. 
+
+### Docs
+
+* [webextension-toolbox](https://github.com/HaNdTriX/webextension-toolbox)
---
+++ app/scripts/contentscript.js
@@ -0,0 +1,42 @@
+let interval = null;
+
+const fixAds = (video) => {
+	if (video == null)
+		return;
+		
+	console.log("[AdSkip] Detected a playing video ...");
+	
+	let parent = video.parentElement.parentElement;
+	let isAd = parent.classList.contains('ad-showing');
+	
+	if (isAd) {
+		console.log('[AdSkip] => Advertisment detected, try to speed up things ...');
+		video.playbackRate = 16;
+		video.muted = true;
+		video.style.opacity = '0.05';
+		video.style.filter = 'blur(50px)';
+		interval = setInterval(() => {
+			let skipButton = document.querySelector('[class*="ytp-ad-skip-button"]');
+			if (skipButton != null) {
+				skipButton.click();
+				console.log('[AdSkip] => Advertisment skip button clicked.');
+			}
+		}, 100);
+	} else {
+		console.log("[AdSkip] => This video is not an ad, do nothing.");
+		clearInterval(interval);
+		video.playbackRate = 1;
+		video.muted = false;
+		video.style.opacity = '1';
+		video.style.filter = '';
+	}
+}
+
+document.body.addEventListener('play', (ev) => {
+	fixAds(ev.target);
+}, true);
+
+document.addEventListener('DOMContentLoaded', () => {
+	let video = document.querySelector('video');
+	fixAds(video);
+});
